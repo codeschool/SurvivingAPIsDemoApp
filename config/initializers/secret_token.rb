f9b7de1331109f1c3421a1d96d504ed01cff09d1
Carlos Souza <carloshrsouza@gmail.com>
2013-12-03 10:40:17 -0500

    Create new Rails 4.0.1 app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BananaPodcast::Application.config.secret_key_base = 'ecf80c76fe1efc8a692a3222d7b9182266ff9ca05db04bbcfd811dc7ef8cd02c065484a7d021b8d751f25ba9fb17f961b8ddfce828e238d7b3d4b65f65000370'
