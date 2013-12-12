24fa65527b6fa1ef3e9e745b9f00e24fe459e51a
Carlos Souza <carloshrsouza@gmail.com>
2013-12-12 15:30:34 -0500

    Add seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+#
+Zombie.create!(name: 'Jon', age: 123)
+Zombie.create!(name: 'Isabella', age: 93)
+
