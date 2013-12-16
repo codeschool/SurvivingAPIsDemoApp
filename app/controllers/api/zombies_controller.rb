a91c0548a57fc0f4e0073aa79d517aec473bd358
Carlos Souza <carloshrsouza@gmail.com>
2013-12-16 15:05:10 -0500

    Add GET specs.

--- app/controllers/api/zombies_controller.rb
+++ app/controllers/api/zombies_controller.rb
@@ -1,9 +1,14 @@
 class API::ZombiesController < ApplicationController
   def index
-    @zombies = Zombie.all # remember, starting in Rails 4 this returns a chainable scope
+    zombies = Zombie.all # remember, starting in Rails 4 this returns a chainable scope
     if weapon = params[:weapon]
-      @zombies = @zombies.where(weapon: weapon)
+      zombies = zombies.where(weapon: weapon)
     end
-    render json: @zombies
+    render json: zombies, status: 200
+  end
+
+  def show
+    zombie = Zombie.find(params[:id])
+    render json: zombie, status: 200
   end
 end
--- spec/requests/listing_zombies_spec.rb
+++ spec/requests/listing_zombies_spec.rb
@@ -6,15 +6,17 @@ describe "Listing Zombies" do
     it "returns successful response" do
       get api_zombies_url
       expect(response.status).to be(200)
+      expect(response).to be_success
+      expect(response).to be_successful
+      expect(response.body).to_not be_empty
     end
 
     context 'filtering by weapon' do
-      before do
-        @john = Zombie.create!(name: 'John', weapon: 'axe')
-        @joanna = Zombie.create!(name: 'Joanna', weapon: 'shotgun')
-      end
 
       it 'returns zombies with specified weapon' do
+        john = Zombie.create!(name: 'John', weapon: 'axe')
+        joanna = Zombie.create!(name: 'Joanna', weapon: 'shotgun')
+
         get api_zombies_url(weapon: 'axe')
         expect(response.status).to be(200)
 
@@ -22,6 +24,15 @@ describe "Listing Zombies" do
         names = zombies.collect { |z| z[:name] }
         expect(names).to include('John')
         expect(names).to_not include('Joanna')
+      end
+
+      it 'returns zombie from id' do
+        zombie = Zombie.create!(name: 'Joanna', weapon: 'axe')
+        get api_zombie_url(zombie)
+        expect(response.status).to be(200)
+
+        zombie_response = JSON.parse(response.body, symbolize_names: true)
+        expect(zombie_response[:name]).to eq(zombie.name)
       end
     end
   end
