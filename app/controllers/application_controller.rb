f8131b5e2bb674cb8adfdbb5e5cfb547c05f1e57
Carlos Souza <carloshrsouza@gmail.com>
2014-01-22 15:42:32 -0500

    Use built in authenticate_or_request_with_http_token.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,19 +1,19 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  before_action :authenticate_user
+  before_action :authenticate
 
   protected
 
-    def authenticate_user
+    def authenticate
       current_user || render_unauthorized
     end
 
     def current_user
-      @current_user ||= User.find_by(auth_token: auth_token)
-    end
-
-    def auth_token
-      request.headers['USER_AUTH_TOKEN']
+      @current_user ||= begin
+                          authenticate_or_request_with_http_token do |token, options|
+                            User.find_by(auth_token: token)
+                          end
+                        end
     end
 
     def render_unauthorized
--- test/integration/listing_payments_test.rb
+++ test/integration/listing_payments_test.rb
@@ -4,19 +4,29 @@ class ListingPaymentsTest < ActionDispatch::IntegrationTest
   setup do
     @user = User.create!(email: 'foo@bar.com')
     @user.payments.create!(amount: 99.99)
+    @auth_header = "Token token=#{@user.auth_token}"
+    @token = ActionController::HttpAuthentication::Token.encode_credentials(@user.auth_token)
   end
 
   teardown do
     User.destroy_all
   end
 
-  test 'valid authentication' do
-    get '/payments', {}, { 'USER_AUTH_TOKEN' => @user.auth_token }
+  # Show this example first
+  test 'valid authentication with manual token' do
+    get '/payments', {}, { 'Authorization' => @auth_header }
     assert_equal 200, response.status
   end
 
+  # Show this after showing the #encode_credentials method
+  test 'valid authentication' do
+    get '/payments', {}, { 'Authorization' => @token }
+    assert_equal 200, response.status
+  end
+
+
   test 'invalid authentication' do
-    get '/payments', {}, { 'USER_AUTH_TOKEN' => @user.auth_token + 'fake' }
+    get '/payments', {}, { 'Authorization' => @token + 'fake' }
     assert_equal 401, response.status
   end
 end
