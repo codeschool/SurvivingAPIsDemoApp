032bd58da9cdebfecf6b3311dcfa2ce1db8bdd1c
Carlos Souza <carloshrsouza@gmail.com>
2014-01-30 18:01:14 -0500

    Missing status code.

--- app/controllers/api/v1/zombies_controller.rb
+++ app/controllers/api/v1/zombies_controller.rb
@@ -3,7 +3,7 @@ module API
     class ZombiesController < VersionController
 
       def index
-        render text: "#{@remote_ip} Version #{@api_version}!"
+        render json: "#{@remote_ip} Version #{@api_version}!", status: 200
       end
     end
   end
