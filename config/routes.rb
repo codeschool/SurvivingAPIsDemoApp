f2dc137b207eed0561d98ad1913536fe183c9c93
Carlos Souza <carloshrsouza@gmail.com>
2014-01-31 14:09:04 -0500

    Add nice-to-have for concerns.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 BananaPodcast::Application.routes.draw do
+
+=begin
+  class Banana
+    def self.call(mapper, options)
+      mapper.namespace :api, path: '/', constraints: { subdomain: 'api' }
+    end
+  end
+
+  concern :api, Banana
+  resources :zombies, concerns: :api
+=end
+
   namespace :api, path: '/', constraints: { subdomain: 'api' } do
     resources :zombies
   end
