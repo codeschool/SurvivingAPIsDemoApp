18856bb46c76e5992bdd1dc461eff93458010b32
Carlos Souza <carloshrsouza@gmail.com>
2014-02-21 13:58:09 -0500

    Extract method for token generation.

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,13 @@ class User < ActiveRecord::Base
 
     def set_auth_token
       return if auth_token.present?
+      self.auth_token = generate_auth_token
+    end
 
-      begin
-        self.auth_token = SecureRandom.hex
-      end while self.class.exists?(auth_token: self.auth_token)
+    def generate_auth_token
+      loop do
+        token = SecureRandom.hex
+        break token unless self.class.exists?(auth_token: token)
+      end
     end
 end
